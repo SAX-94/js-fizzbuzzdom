60ad507b0990ef79c38e4a44f2e2cb1fb5e0284d
SAX_94 <matteosacchi94@gmail.com>
2022-05-20 14:41:19 +0200

    added Fizz, Buzz and FizzBuzz script

---
+++ js/fizzbuzz.js
@@ -0,0 +1,18 @@
+/* Se i è divisibile per 3 (i % 3 === 0), allora console.log mostrerà "Fizz".
+Analogo ragionamento per 5, ma con la parola "Buzz". Se un numero è divisibile
+sia per 3, sia per 5, allora è divisiile per 15. In tal caso, verrà mostrata la
+parola FizzBuzz. */ 
+
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else if (i % 15 === 0){
+        console.log("FizzBuzz");
+    } else {
+        console.log(i);
+    }
+}
