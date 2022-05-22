bc8ee513345cd2d0aa2827d43415e674074aa04a
SAX_94 <matteosacchi94@gmail.com>
2022-05-22 23:45:31 +0200

    edited script

--- js/editDom.js
+++ js/editDom.js
@@ -1,16 +1,15 @@
 
-let spanText = document.getElementsByClassName("importedHtml");
+let spanText = document.querySelectorAll('.importedHtml')
+
 
 for (let i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
-        spanText = "FizzBuzz";
+        console.log("FizzBuzz");
     } else if (i % 5 == 0) {
-        spanText = "Buzz";
+        console.log("Buzz");
     } else if (i % 3 == 0) {
-        spanText = "Fizz";
+        console.log("Fizz");
     } else {
         console.log(i);
     }
 }
-
-
