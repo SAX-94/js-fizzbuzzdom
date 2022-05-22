9c6bdf77e963e9a6008bcb5437200f6091874a62
SAX_94 <matteosacchi94@gmail.com>
2022-05-22 22:56:36 +0200

    corrected this script, and now it works properly

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -6,12 +6,12 @@ parola FizzBuzz. */
 
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else if (i % 15 === 0){
+    if (i % 15 == 0) {
         console.log("FizzBuzz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
     } else {
         console.log(i);
     }
