51c1e5398d1fb90ee6fa847093af2b73c2a612d1
SAX_94 <matteosacchi94@gmail.com>
2022-05-23 00:18:25 +0200

    correced script link and commented it again

--- js/editDom.js
+++ js/editDom.js
@@ -1,15 +1,20 @@
 
-let spanText = document.querySelectorAll('.importedHtml')
+let spanText = document.querySelectorAll('.importedHtml');
+
+
+
+/* Devo usare .textContent sugli spanText per metterci dentro
+del testo, a seconda delle condizoni del ciclo */
 
 
 for (let i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
-        console.log("FizzBuzz");
+        spanText.textContent = "FizzBuzz";
     } else if (i % 5 == 0) {
-        console.log("Buzz");
+        spanText.textContent = "Buzz";
     } else if (i % 3 == 0) {
-        console.log("Fizz");
+        spanText.textContent = "Fizz";
     } else {
-        console.log(i);
+        spanText.textContent = i;
     }
 }
